7fa77dfee4f42b5a8617dc7af767a43855abe973
willenwu <willenwu21@gmail.com>
2024-01-16 09:48:34 +0800

    add HostlistCompiler config add RemoveEmptyLines

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+# -*- coding: utf-8 -*-
 import os
 import os.path
 import time
 import requests
 import shutil
 
-_my_allow = 'my-allow.txt'
-_merge_txt = 'merge.txt'
 _base_dir = os.getcwd()
 _rule_dir = _base_dir + '/rules'
+_my_allow_f = 'my-allow.txt'
+_merge_txt_f = 'merge.txt'
+_download_list = [
+    'https://easylist.to/easylist/easylist.txt',
+    'https://raw.githubusercontent.com/easylist/easylistchina/master/easylistchina.txt',
+    'https://easylist.to/easylist/easyprivacy.txt',
+    'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt',
+    'https://github.com/badmojr/1Hosts/releases/download/latest/1hosts-Lite_adblock.txt',
+    'https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt',
+    'https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt',
+    'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt',
+    'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',
+    'https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt',
+    'https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt'
+]
 
 
-def is_dir_empty(dir_path):
-    return not bool(os.listdir(dir_path))
-
-
-def _merge(filedir):
-    print('Start merge path : %s ' % filedir)
-    filelist = os.listdir(filedir)
-    with open(_base_dir + '/' + _merge_txt, 'w', encoding='utf-8') as f:
+def _merge(rule_dir):
+    print('Start merge path : %s ' % rule_dir)
+    filelist = os.listdir(rule_dir)
+    with open(_base_dir + '/' + _merge_txt_f, 'w', encoding='utf-8') as f:
         for filename in filelist:
-            filepath = filedir + '/' + filename
+            filepath = rule_dir + '/' + filename
             for line in open(filepath, encoding='utf-8'):
                 f.writelines(line)
             f.write('\n')
@@ -33,53 +43,46 @@ def _merge_allow(file1, file2):
             f1.write(i)
 
 
-def _download(url, outdir):
-    print('Start download file from url %s ' % url)
-    filename = outdir + '/' + os.path.basename(i)
-    res = requests.get(url)
-    with open(filename, 'wb') as f:
-        f.write(res.content)
+def _check_rule_dir(rule_dir):
+    if not os.path.exists(rule_dir):
+        os.mkdir(rule_dir)
+        print('rules dir empty, create : %s' % rule_dir)
 
 
-def _clean_dir(rule_dir):
-    # 判断文件夹是否为空
+def _download(file_list, rule_dir):
+    _check_rule_dir(rule_dir)
+    for i in file_list:
+        print('Start download file from url %s ' % i)
+        filename = _rule_dir + '/' + os.path.basename(i)
+        res = requests.get(i)
+        with open(filename, 'wb') as f:
+            f.write(res.content)
+
+
+def _clean_rule_dir(rule_dir):
     if os.path.exists(rule_dir):
         print('rules dir exist, delete : %s' % rule_dir)
         shutil.rmtree(rule_dir)
 
 
-if __name__ == '__main__':
-    # 开始
-    start = time.process_time()
-    list = [
-        'https://easylist-downloads.adblockplus.org/easylist.txt',
-        'https://easylist-downloads.adblockplus.org/easylistchina.txt',
-        'https://easylist-downloads.adblockplus.org/easyprivacy.txt',
-        'https://github.com/badmojr/1Hosts/releases/download/latest/1hosts-Xtra_adblock.txt',
-        'https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt',
-        'https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt',
-        'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt',
-        'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',
-        'https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/all.txt',
-        'https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/skyrules.txt',
-        'https://raw.githubusercontent.com/BlueSkyXN/AdGuardHomeRules/master/ok.txt'
-    ]
-    rule_dir = os.getcwd()+'/rules'
-    # 清理文件
-    _clean_dir(_rule_dir)
-    os.mkdir(_rule_dir)
-    print('rules dir empty, create : %s' % _rule_dir)
-    # 下载
-    for i in list:
-        print('URI: %s' % i)
-        _download(i, _rule_dir)
-        print('URI download done: %s' % i)
+def _del_merge_file(_merge_file):
+    if os.path.exists(_merge_file):
+        print('rules dir exist, delete : %s' % _merge_file)
+        os.remove(_merge_file)
 
-    # 合并
+
+if __name__ == '__main__':
+    # Start
+    start = time.process_time()
+    _download(_download_list, _rule_dir)
+    # merge download rules
     _merge(_rule_dir)
-    _merge_allow(_base_dir + '/' + _merge_txt, _base_dir + '/' + _my_allow)
-    # 清理文件
-    _clean_dir(rule_dir)
-    # 结束
+    # merge my-allow
+    _merge_allow(_base_dir + '/' + _merge_txt_f, _base_dir + '/' + _my_allow_f)
+    # clean rules dir
+    _clean_rule_dir(_rule_dir)
+    # delete merge.txt
+    _del_merge_file(_base_dir + '/' + _merge_txt_f)
+    # end
     end = time.process_time()
     print('Running time : %s Seconds' % (end-start))
